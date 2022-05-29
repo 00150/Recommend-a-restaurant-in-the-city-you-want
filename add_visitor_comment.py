cb149b0f073d5e270eb2fec9e9bf525743dd8714
00150 <trendyou@naver.com>
2022-05-29 12:49:35 +0900

    try to make function of searching all comment

--- add_visitor_comment.py
+++ add_visitor_comment.py
@@ -55,46 +55,58 @@ def add_comment_only_one(df):
 
 # --------------------🔱 생성된 함수를 실행하기 위해선 다음과 같은 코드를 실행해야 합니다. 🔱------------------------
 
-#df1 = pd.read_csv('add_store_score.csv', encoding='cp949')
-#add_comment_only_one(df1)
+# df1 = pd.read_csv('add_store_score.csv', encoding='cp949')
+# add_comment_only_one(df1)
 
 
 
+#-------------------- 🔰 part2.  🔰 ------------------------
+#이번에는 모든 리뷰를 가져올 수 있게 시도하여 봅시다.
 
 
-# -------------------- 🔰 part2.  🔰 ------------------------
-# 이번에는 리뷰중 5개의 리뷰만 가져올 수 있게 노력해봅시다.
-
-
+df1 = pd.read_csv('add_store_score.csv', encoding='cp949')
 # 셀레니움 드라이브 연결
 s = Service(r'C:\Users\j.park\Section3\real_project3\chromedriver.exe')
 comment_driver = webdriver.Chrome(service=s)
 
-
 # 크롤링한 데이터를 모으기 위한 빈 리스트 생성
 visitor_comment_list = []
 
-# df 불러오기.
-df1 = pd.read_csv('add_store_score.csv', encoding='cp949')
 
+# 임시 저장소
+temporary_list = []
 
 #df_column : df['naver_store_url'] 이용
 for i, url in enumerate(df1['naver_store_url']):
-            
+          
   # 크롤링할 리뷰가 존재하는 url을 드라이버에 연결합니다.
   comment_driver.get(url+'/review/visitor?reviewItem=0')
-  try:
-    visitor_review = comment_driver.find_element_by_class_name("WoYOw").text
-    visitor_comment_list.append(visitor_review)
+  #try:
   
-  except Exception as e1:
-    print(f'{i}행의 리뷰가 존재하지 않습니다.')
-    visitor_review = 'None comment by visitor'
-    visitor_comment_list.append(visitor_review)
+    #전체 리뷰 텍스트 가져오기
+    #참고 : find_elements_by 로 가져오는 내용은 리스트로 저장됩니다. 
+  visitor_review =temporary_list.append(comment_driver.find_elements_by_class_name("WoYOw"))
 
-visitor_comment_list
+breakpoint()    
+#     for elements in visitor_review:
+#       reviews = ','.join(elements)
+#       visitor_comment_list.append(reviews)
 
-    
+
+#   except Exception as e1:
+#     print(f'{i}행의 리뷰가 존재하지 않습니다.')
+#     visitor_review = 'None comment by visitor'
+#     visitor_comment_list.append(visitor_review)
+
+# comment_driver.quit()
+# df1['all_review_by_visitor'] = visitor_comment_list
+
+# return None
+
+
+
+# df1 = pd.read_csv('add_store_score.csv', encoding='cp949')
+# all_review_select(df1)
     
     
 #app-root > div > div > div > div:nth-child(7) > div > div.place_section._3fSeV > div > ul > li:nth-child(1) > div.faZHB > a > span  
